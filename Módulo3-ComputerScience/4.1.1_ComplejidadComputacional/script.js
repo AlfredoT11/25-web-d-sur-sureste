558b5ef9d1fa19ad6ed565bb1720764d43579c0a
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2023-06-19 20:51:55 -0600

    Recursividad y comparación de algoritmos de ordenamiento

--- Módulo3-ComputerScience/4.1.1_ComplejidadComputacional/script.js
+++ Módulo3-ComputerScience/4.1.1_ComplejidadComputacional/script.js
@@ -17,12 +17,40 @@ function generarAleatorios(n, numeroMaximo){
     return Array.from({ length: n }, () => Math.floor(Math.random() * numeroMaximo));
 }
 
-let arregloBurbuja = generarAleatorios(100, 100);
-let arregloBurbujaOrdenado = ordenamientoBurbuja(arregloBurbuja);
+let tiempoInicial;
+let tiempoFinal;
 
+let arregloBurbuja = generarAleatorios(20000, 100);
+tiempoInicial = Date.now();
+let arregloBurbujaOrdenado = ordenamientoBurbuja(arregloBurbuja);
+tiempoFinal = Date.now();
+
+console.log(`Burbuja: ${tiempoFinal - tiempoInicial} ms`);
+
+/*
 console.log(`Arreglo sin ordenar: `);
 console.log(arregloBurbuja);
 
 console.log(`Arreglo ordenado: `);
 console.log(arregloBurbujaOrdenado);
+*/
+
+let arregloNativo = generarAleatorios(20000, 100);
+
+tiempoInicial = Date.now();
+arregloNativo.sort((a, b) => {return a-b});
+tiempoFinal = Date.now();
+console.log(`Usando sort JS: ${tiempoFinal - tiempoInicial} ms`);
+
+function factorial(n){
+    if(n == 1){
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+let valorFactorial = 5;
+let factorialResultado = factorial(valorFactorial);
+console.log(`El factorial de ${valorFactorial} es: ${factorialResultado}`);
+
 
