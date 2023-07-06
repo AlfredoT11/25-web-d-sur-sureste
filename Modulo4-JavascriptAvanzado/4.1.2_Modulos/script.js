9603b10017bca69986d80127e91b8584aa12b9b8
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2023-07-05 20:03:08 -0600

    Recordatorio de desestructuración.

--- Modulo4-JavascriptAvanzado/4.1.2_Modulos/script.js
+++ Modulo4-JavascriptAvanzado/4.1.2_Modulos/script.js
@@ -1,5 +1,5 @@
 const { PI, calcularAreaCirculo } = require('./moduloA');
-const constantesFisicas1 = require('./constantesFisicas');
+const constantesFisicas1 = require('./misModulos/constantesFisicas');
 const { funcionesGeometricas } = require('./misModulos/areasFiguras');
 
 
@@ -13,3 +13,21 @@ console.log(`El valor de la variable importada es: ${funcionesGeometricas.miVari
 
 let areaDelRectangulo = funcionesGeometricas.calcularAreaRectangulo(5, 4);
 console.log(`El area del rectangulo es: ${areaDelRectangulo}`);
+
+// Recordatorio de desestructuración.
+let persona = {
+    nombre: 'Juan',
+    edad: 28,
+    estatura: 1.45
+};
+
+/*
+let nombre = persona.nombre;
+let edad = persona.edad;
+let estatura = persona.estatura;
+*/
+
+// Esta línea es el equivalente a las 3 líneas superiores comentadas.
+let { nombre, edad, estatura } = persona;
+
+console.log(`El nombre es: ${nombre}`);
