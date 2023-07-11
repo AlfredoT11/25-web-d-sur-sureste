3404a6b5ff01430190d668d97316d2f0aedd1ffb
Francisco Campos <sasasasamaes13@gmail.com>
2023-07-10 19:38:52 -0600

    Callbacks

---
+++ Modulo4-JavascriptAvanzado/4.1.3_Callbacks/index.js
@@ -0,0 +1,25 @@
+
+
+function saludar(nombre) {
+    console.log(`Hola Mundo ${nombre}}`)
+}
+
+function saludar2(nombre="") {
+  console.log(`Hola Dev.f`)
+}
+
+// setTimeout(saludar, 5000)
+
+
+// Ejemplo 2
+
+function procesarEntradaUsuario(callback) {
+    let nombre = prompt('Dime tu nombre')
+    setTimeout(callback(nombre), 5000)
+    callback(nombre)
+}
+
+
+procesarEntradaUsuario(saludar)
+procesarEntradaUsuario(saludar2)
+
